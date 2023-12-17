07892193bb1faf6a22b695db541caa10c969bef0
Jun <junyynyyn@users.noreply.github.com>
2023-12-17 11:29:50 +0800

    Added scoring, ball velocity increase over time

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -4,6 +4,7 @@ var BASE_SPEED = 200
 var SPEED
 var LOAD_POSITION: Vector2
 var resetState = false
+var increaseSpeed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,8 +18,15 @@ func _process(_delta):
 func _integrate_forces(state: PhysicsDirectBodyState2D):
 	if resetState == true:
 		state.transform.origin = LOAD_POSITION
+		state.linear_velocity = Vector2(BASE_SPEED, BASE_SPEED)
 		resetState = false
+	if increaseSpeed == true:
+		state.linear_velocity *= 1.1
+		increaseSpeed = false
 
 func reset():
 	SPEED = BASE_SPEED
 	resetState = true
+
+func _on_timer_timeout():
+	increaseSpeed = true
--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,7 +1,8 @@
 extends StaticBody2D
 
 @export var player: bool
-var MOVE_SPEED = 150.0
+@export var playernum: int
+var MOVE_SPEED = 200.0
 var SIZE = 100
 
 # Called when the node enters the scene tree for the first time.
@@ -11,10 +12,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (player):
-		if (Input.is_action_pressed("up")):
-			if (position.y >= SIZE + 20):
-				position.y -= MOVE_SPEED * delta
-		if (Input.is_action_pressed("down")):
-			if (position.y  <= get_viewport().size.y - SIZE - 20):
-				position.y += MOVE_SPEED * delta
-			
+		if (playernum == 0):
+			if (Input.is_action_pressed("up")):
+				if (position.y >= SIZE + 20):
+					position.y -= MOVE_SPEED * delta
+			if (Input.is_action_pressed("down")):
+				if (position.y  <= get_viewport().size.y - SIZE - 20):
+					position.y += MOVE_SPEED * delta
+		elif (playernum == 1):
+			if (Input.is_action_pressed("up2")):
+				if (position.y >= SIZE + 20):
+					position.y -= MOVE_SPEED * delta
+			if (Input.is_action_pressed("down2")):
+				if (position.y  <= get_viewport().size.y - SIZE - 20):
+					position.y += MOVE_SPEED * delta
